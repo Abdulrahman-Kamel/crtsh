1bab66ec3b3054a99ec3f1905462eb29d0301a88
Abdulrahman-Kamel <49864433+Abdulrahman-Kamel@users.noreply.github.com>
2021-07-15 09:41:40 +0200

    Create crtsh.sh

---
+++ crtsh.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# bash tool => return all clear subdomains
+# bash tool "*" => return all subdomains which have astric
+
+domain=$1
+subs_all=()
+subs_astric=()
+subs_normal=()
+
+getSubs() {
+	subs_all=$(curl -s https://crt.sh/?q=%.$domain | grep $domain | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | grep $domain | sort -u)
+}
+getSubs
+
+for sub in $subs_all; do
+	if [[ "$sub" =~ "*" ]]; then
+		subs_astric+="$sub "
+	else
+		subs_normal+="$sub "
+	fi
+done
+
+if [[ $2 == "*" ]]; then
+	for sub in $subs_astric; do
+		echo $sub
+	done
+else
+	for sub in $subs_normal; do
+		echo $sub
+	done
+
+fi
